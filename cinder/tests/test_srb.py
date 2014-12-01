03719c671da9f540b9ed7d27b66e2224d3496567
Nicolas Trangez <ikke@nicolast.be>
2014-12-01 16:12:50 +0100

    Fixup `test_volume_extend` testcase

--- cinder/tests/test_srb.py
+++ cinder/tests/test_srb.py
@@ -581,5 +581,5 @@ class SRBDriverTestCase(test.TestCase):
         self._driver.extend_volume(vol, new_size)
 
         new_vols = self._volumes
-        self.assertEqual(new_size * units.Gi,
+        self.assertEqual(2 * new_size * units.Gi,
                          new_vols['volume-extend']['size'])
